19dcd296509d89e0ad17f09d6bd842466676a69c
Ivan Vakhrushev <37612014+mfvanek@users.noreply.github.com>
2023-06-02 09:31:31 +0400

    Migration to Gradle (#29)
    * Added Gradle wrapper
    
    * Migrated to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,161 @@
+import com.github.spotbugs.snom.Confidence
+import com.github.spotbugs.snom.Effort
+import com.github.spotbugs.snom.SpotBugsTask
+import net.ltgt.gradle.errorprone.errorprone
+
+plugins {
+    id("java-library")
+    id("jacoco")
+    id("maven-publish")
+    id("checkstyle")
+    id("pmd")
+    id("com.github.spotbugs") version "5.0.14"
+    id("net.ltgt.errorprone") version "3.1.0"
+    id("io.freefair.lombok") version "8.0.1"
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+group = "io.github.mfvanek"
+version = "1.5.0-SNAPSHOT"
+description = "Simple implementation of two levels caching"
+
+dependencies {
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("org.slf4j:slf4j-api:2.0.7")
+    implementation("ch.qos.logback:logback-classic:1.4.7")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.3"))
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+
+    //pitest("it.mulders.stryker:pit-dashboard-reporter:0.2.1")
+    checkstyle("com.thomasjensen.checkstyle.addons:checkstyle-addons:7.0.1")
+    errorprone("com.google.errorprone:error_prone_core:2.19.1")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    withJavadocJar()
+    withSourcesJar()
+}
+tasks.withType<JavaCompile>().configureEach {
+    options.errorprone {
+        disableWarningsInGeneratedCode.set(true)
+    }
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+        dependsOn(checkstyleMain, checkstyleTest, pmdMain, pmdTest, spotbugsMain, spotbugsTest)
+        finalizedBy(jacocoTestReport, jacocoTestCoverageVerification)
+    }
+
+    javadoc {
+        if (JavaVersion.current().isJava9Compatible) {
+            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+        }
+    }
+
+    jacocoTestReport {
+        dependsOn(test)
+        reports {
+            xml.required.set(true)
+            html.required.set(true)
+        }
+    }
+
+    jacocoTestCoverageVerification {
+        dependsOn(test)
+        violationRules {
+            rule {
+                limit {
+                    counter = "CLASS"
+                    value = "MISSEDCOUNT"
+                    maximum = "1".toBigDecimal()
+                }
+            }
+            rule {
+                limit {
+                    counter = "METHOD"
+                    value = "MISSEDCOUNT"
+                    maximum = "27".toBigDecimal() // +1 from windows build
+                }
+            }
+            rule {
+                limit {
+                    counter = "LINE"
+                    value = "MISSEDCOUNT"
+                    maximum = "90".toBigDecimal() // +2 from windows build
+                }
+            }
+            rule {
+                limit {
+                    counter = "BRANCH"
+                    value = "COVEREDRATIO"
+                    minimum = "0.78".toBigDecimal()
+                }
+            }
+            rule {
+                limit {
+                    counter = "INSTRUCTION"
+                    value = "COVEREDRATIO"
+                    minimum = "0.78".toBigDecimal()
+                }
+            }
+        }
+    }
+
+    check {
+        dependsOn(jacocoTestReport, jacocoTestCoverageVerification)
+    }
+}
+
+lombok {
+    version.set(libs.versions.lombok.get())
+}
+
+jacoco {
+    toolVersion = libs.versions.jacoco.get()
+}
+
+checkstyle {
+    toolVersion = libs.versions.checkstyle.get()
+    configFile = file("config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
+    maxWarnings = 0
+    maxErrors = 0
+}
+
+pmd {
+    toolVersion = libs.versions.pmd.get()
+    isConsoleOutput = true
+    ruleSetFiles = files("config/pmd/pmd.xml")
+    ruleSets = listOf()
+}
+
+spotbugs {
+    showProgress.set(true)
+    effort.set(Effort.MAX)
+    reportLevel.set(Confidence.LOW)
+    excludeFilter.set(file("config/spotbugs/exclude.xml"))
+}
+tasks.withType<SpotBugsTask>().configureEach {
+    reports {
+        create("xml") { enabled = true }
+        create("html") { enabled = true }
+    }
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
